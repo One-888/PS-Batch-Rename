d2c6d29a1c8eee4d57bb9708e134bb271bb183a1
One-888 <53633937+One-888@users.noreply.github.com>
2022-04-10 03:22:24 -0400

    Create Rename_with_Hash_Only.ps1

---
+++ Rename_with_Hash_Only.ps1
@@ -0,0 +1,16 @@
+function batch-rename-files { param ([String] $Path)
+    $i = 0
+	cd $Path 
+
+   $stat= (gci -file )
+
+	gci -file | ` 
+	ForEach-Object { `
+        $i = $i + 1; $pct = [math]::Round(($i / $stat.Count)*100) ; `
+		Write-Progress -Activity "Renaming..." -PercentComplete $pct; `
+		$h = (hash-it $_.FullName) ;`
+		$newname = $h + ($_.Extension).ToUpper(); ` 
+		Rename-Item -Path $_.FullName -Newname $newname ; `
+		return ( "`n" + $_.Name + " > " + $newname ); `` 
+	}
+}
