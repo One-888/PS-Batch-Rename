773c7b697122f9e83904f599864e1ddc206396ab
One-888 <53633937+One-888@users.noreply.github.com>
2022-05-31 14:42:24 -0400

    Update Rename_with_Hash_Only.ps1

--- Rename_with_Hash_Only.ps1
+++ Rename_with_Hash_Only.ps1
@@ -1,16 +1,18 @@
 function batch-rename-files { param ([String] $Path)
     $i = 0
 	cd $Path 
-
-   $stat= (gci -file )
-
+    
+    $stat = gci -file
+     
 	gci -file | ` 
 	ForEach-Object { `
         $i = $i + 1; $pct = [math]::Round(($i / $stat.Count)*100) ; `
 		Write-Progress -Activity "Renaming..." -PercentComplete $pct; `
-		$h = (hash-it $_.FullName) ;`
+		$h = (hash-it $_.FullName);`
 		$newname = $h + ($_.Extension).ToUpper(); ` 
 		Rename-Item -Path $_.FullName -Newname $newname ; `
 		return ( "`n" + $_.Name + " > " + $newname ); `` 
 	}
 }
+
+# batch-rename-files -Path  "C:\iCloud Photos" 
